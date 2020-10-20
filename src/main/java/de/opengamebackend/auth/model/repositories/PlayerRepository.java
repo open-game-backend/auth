2a06fe202a3bde2bd335d9c796a51fd7c3b148d6
Nick Prühs <dev@npruehs.de>
2020-10-20 23:21:34 +0200

    Add @Repository annotations.

--- src/main/java/de/opengamebackend/auth/model/repositories/PlayerRepository.java
+++ src/main/java/de/opengamebackend/auth/model/repositories/PlayerRepository.java
@@ -2,6 +2,8 @@ package de.opengamebackend.auth.model.repositories;
 
 import de.opengamebackend.auth.model.entities.Player;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends CrudRepository<Player, String> {
 }
--- src/main/java/de/opengamebackend/auth/model/repositories/RoleRepository.java
+++ src/main/java/de/opengamebackend/auth/model/repositories/RoleRepository.java
@@ -2,7 +2,9 @@ package de.opengamebackend.auth.model.repositories;
 
 import de.opengamebackend.auth.model.entities.Role;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends CrudRepository<Role, Long> {
     Role findByName(String name);
 }
