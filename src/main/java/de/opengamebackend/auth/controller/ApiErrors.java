5410ddae3ac0d6730f3a316f63b66597e4d36ecd
Nick Prühs <dev@npruehs.de>
2020-10-24 22:24:17 +0200

    Remove unused error code.

--- src/main/java/de/opengamebackend/auth/controller/ApiErrors.java
+++ src/main/java/de/opengamebackend/auth/controller/ApiErrors.java
@@ -5,6 +5,4 @@ import de.opengamebackend.net.ApiError;
 public class ApiErrors {
     public static final ApiError ERROR_INVALID_CREDENTIALS =
             new ApiError(100, "Invalid credentials.");
-    public static final ApiError ERROR_INVALID_ACCESS_TOKEN =
-            new ApiError(101, "Invalid access token.");
 }
