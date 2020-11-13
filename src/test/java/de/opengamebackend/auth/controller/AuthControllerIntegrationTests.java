a25afa6d8d48e204f901b64c3d4831e65138fb37
Nick Prühs <dev@npruehs.de>
2020-11-13 23:55:16 +0100

    Use new test utility package for integration tests.

--- src/test/java/de/opengamebackend/auth/controller/AuthControllerIntegrationTests.java
+++ src/test/java/de/opengamebackend/auth/controller/AuthControllerIntegrationTests.java
@@ -1,67 +1,51 @@
 package de.opengamebackend.auth.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import de.opengamebackend.auth.model.entities.Player;
 import de.opengamebackend.auth.model.requests.LoginRequest;
 import de.opengamebackend.auth.model.requests.RegisterRequest;
 import de.opengamebackend.auth.model.responses.LoginResponse;
 import de.opengamebackend.auth.model.responses.RegisterResponse;
+import de.opengamebackend.test.HttpRequestUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestEntityManager
 @Transactional
 public class AuthControllerIntegrationTests {
-    @Autowired
     private MockMvc mvc;
+    private TestEntityManager entityManager;
+    private HttpRequestUtils httpRequestUtils;
 
     @Autowired
-    private TestEntityManager entityManager;
+    public AuthControllerIntegrationTests(MockMvc mvc, TestEntityManager entityManager) {
+        this.mvc = mvc;
+        this.entityManager = entityManager;
+
+        this.httpRequestUtils = new HttpRequestUtils();
+    }
 
     @Test
     public void whenRegister_thenOk() throws Exception {
         RegisterRequest request = new RegisterRequest("testNickname");
-        httpPost("/register", request, RegisterResponse.class);
+        httpRequestUtils.assertPostOk(mvc, "/register", request, RegisterResponse.class);
     }
 
     @Test
     public void givenPlayer_whenLogin_thenOk() throws Exception {
         Player player = new Player("testId", "testNickname");
-        entityManager.persist(player);
-        entityManager.flush();
+        entityManager.persistAndFlush(player);
 
         LoginRequest request = new LoginRequest(player.getPlayerId());
-        httpPost("/login", request, LoginResponse.class);
-    }
-
-    private <T> T httpPost(String url, Object request, Class<T> responseClass) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestJson = objectMapper.writeValueAsString(request);
-
-        String responseJson = mvc.perform(post(url)
-                .content(requestJson)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse().getContentAsString();
-
-        T response = objectMapper.readValue(responseJson, responseClass);
-        assertThat(response).isNotNull();
-
-        return response;
+        httpRequestUtils.assertPostOk(mvc, "/login", request, LoginResponse.class);
     }
 }
