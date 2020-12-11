d6a2750c06c5cc8d6dd453bbf28d9dac0f75e92c
Nick Prühs <dev@npruehs.de>
2020-12-11 21:02:53 +0100

    Use util library for retrieving application version.

--- src/main/java/de/opengamebackend/auth/AuthApplication.java
+++ src/main/java/de/opengamebackend/auth/AuthApplication.java
@@ -1,20 +1,15 @@
 package de.opengamebackend.auth;
 
 import de.opengamebackend.util.EnableOpenGameBackendUtils;
+import de.opengamebackend.util.config.ApplicationConfig;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
-
-import java.io.FileReader;
-import java.io.IOException;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -26,13 +21,10 @@ public class AuthApplication {
 	}
 
 	@Bean
-	public OpenAPI customOpenAPI() throws IOException, XmlPullParserException {
-		MavenXpp3Reader reader = new MavenXpp3Reader();
-		Model model = reader.read(new FileReader("pom.xml"));
-
+	public OpenAPI customOpenAPI(ApplicationConfig applicationConfig) {
 		return new OpenAPI().info(new Info()
 				.title("Open Game Backend Auth")
-				.version(model.getVersion())
+				.version(applicationConfig.getVersion())
 				.description("Provides authentication and authorization for all players of the Open Game Backend.")
 				.license(new License().name("MIT").url("https://github.com/open-game-backend/auth/blob/develop/LICENSE")));
 	}
